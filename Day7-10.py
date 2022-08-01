137bc3db2e9d94bea410cb4bd56455d131a69664
RIKE5H <rikeshmahat.96@gmail.com>
2022-08-01 15:19:31 +0545

    blackjack and calculator

--- Day7-10.py
+++ Day7-10.py
@@ -1,3 +1,4 @@
+import random
 # simple calculator but with a small tweak the previous calculation can be continued
 def add(num1, num2):
     return num1 + num2
@@ -41,4 +42,77 @@ while is_continued:
     else:
         is_continued = False
 
+
+A = 1
+J = K = Q = 10
+cards = [A, 2,3,4,5,6,7,8,9,10,J, K, Q]
+user_cards = []
+dealer_cards = []
+
+# idk if the below rules complies with the official rule but just made it so doesnot choose the same card
+# to remove it you can just add another len(user_cards) == len(cards): next_card = random.choice(cards)
+# who fking take so many card without going over 21
+def choose_for_user():
+    while True:
+        next_card = random.choice(cards)
+        if next_card not in user_cards:
+            return next_card
+        else:
+            continue
+def choose_for_computer():
+    while True:
+        next_card = random.choice(cards)  
+        if next_card not in dealer_cards:
+            return next_card
+        else:
+            continue
+        
+
+def blackjack():
+
+    first_cards = random.choices(cards, weights=None, k=2)
+    second_card = random.choices(cards, weights=None,k=2)
+    
+    user_cards.extend(first_cards)
+    dealer_cards.extend(second_card)
+    
+    print(f"Your cards : {user_cards}\nDealer Cards : {dealer_cards[0]}")
+    
+    playing =  True
+    while playing:
+        if input("Type 'y' to get another card or 'n' to pass: ") == 'y':
+            user_cards.append(choose_for_user())
+            dealer_cards.append(choose_for_computer())
+            print(f"Your cards : {user_cards}\nDealer Cards : {dealer_cards[0:len(dealer_cards)-1]}")
+        else:
+            playing = False
+
+    print("\n")         
+    user_total = sum(user_cards)
+    dealers_total = sum(dealer_cards)
+    print(f"Your total = {user_total} computer totals = {dealers_total}")
+    if dealers_total != user_total:
+        if user_total > 21 :
+            print("You went over 21 you lose the game")
+        elif user_total == 21:
+            print("Congratulation blackjack, you win")
+        else:
+            if user_total > 21:
+                print("Dealer went over you lose")
+            else:
+                if dealers_total < 21 and dealers_total < user_total:
+                    print("Dealer lost the game, Cogratulation you win")
+                elif dealers_total == 21:
+                    print("black jack dealer won the game")
+                else:
+                    print("Dealer won the game")
+    else:
+        print("its a tie") 
+
+while True:
+    if input("do you want to play the black jack game 'y' for yes 'n' for no: ") == 'y':
+        blackjack()
+    else:
+        print("have a good day\n")
+        break
     
